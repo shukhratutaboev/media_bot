10548236c73f359fcc365b47b46bf1c350ebdc81
shukhratutaboev <shuhratutaboyev2597@gmail.com>
2022-01-29 16:33:12 +0500

    Bot 0 daraja

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using media_bot.Services;
+using Telegram.Bot;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton<TelegramBotClient>(b => new TelegramBotClient(builder.Configuration.GetConnectionString("Token")));
+builder.Services.AddHostedService<Bot>();
+builder.Services.AddTransient<BotHandlers>();
+
+var app = builder.Build();
+
+app.Run();
